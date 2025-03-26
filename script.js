621e400b953fe95270571d08abb51bd1d2d2abf8
Laurenda Edorh <laurenda98@gmail.com>
2025-03-26 19:46:10 +0100

    mes fichiers de bases

---
+++ script.js
@@ -0,0 +1,17 @@
+//variables
+let toggleBtn= document.getElementById("toggle-btn");
+let body = document.querySelector("body");
+let mainImage= document.getElementById("mainImg");
+let text= document.getElementById("text");
+let darkmoDE= localStorage.getItem("dark-mode")
+
+const enableDarkMode = ()=>
+{
+    body.classList.add()
+
+}
+
+const disableDarkMode = ()=>
+{
+    
+}
